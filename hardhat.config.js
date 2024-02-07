b12364fdb43004354e67c10c43761cebcc4eb5cc
Dave Hardin <dave@upperbay.com>
2024-02-07 16:51:16 -0500

    added volta and ewc

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,6 +11,14 @@ module.exports = {
     goerli: {
       url: "https://eth-goerli.public.blastapi.io",
       accounts: ["0x9e07a5d23dd4eb1a551bcbe7364deb089d991aeb5ec6b323f71d9329b71a3bd4"]
+    },
+    volta: {
+      url: "https://volta-rpc.energyweb.org",
+      accounts: ["0x0949a8d20891952dbc52ec59a2aaf36dcd97b5a114103ba4c949fdc0652a2a7f"]
+    },
+    ewc: {
+      url: "https://rpc.energyweb.org",
+      accounts: ["0x0949a8d20891952dbc52ec59a2aaf36dcd97b5a114103ba4c949fdc0652a2a7f"]
     }
   }
 };
